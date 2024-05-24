992c8583da50f37de1a87ce0f035b47b9275b949
Stephanie Carvalho <ste.aoc@gmail.com>
2024-05-23 21:51:36 -0300

    criacao menu responsivo

---
+++ src/JS/script.js
@@ -0,0 +1,13 @@
+function clickMenu(){
+    if (mobileMenu.style.display == 'flex'){
+        mobileMenu.style.display = 'none';
+    }else{
+        mobileMenu.style.display = 'flex';
+    }
+}
+
+
+/*
+botao.addEventListener("click", () => {
+    opcoesMenu.classList.toggle("ativo");
+});*/
